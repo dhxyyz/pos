abb59ddd43d0aa684b3611efcafe01e09c07bd97
dhxyyz <hd964660612@outlook.com>
2015-05-19 16:21:16 +0800

    pos

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,4 +1,13 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
 
+    var goods='***<没钱赚商店>购物清单***\n',a=[],footing=0;
+
+    for (var i = 0; i < inputs.length; i++) {
+        goods=goods+('名称：'+inputs[i].name+'，数量：'+inputs[i].count+ inputs[i].unit+'，单价：'+ inputs[i].price.toFixed(2) +'(元)' +'，小计：'+(inputs[i].count*inputs[i].price).toFixed(2)+'(元)\n');
+        footing=footing+inputs[i].count*inputs[i].price;
+
+    }
+    console.log(goods+'----------------------\n'+'总计：'+footing.toFixed(2)+'(元)\n' +'**********************');
+
 }
